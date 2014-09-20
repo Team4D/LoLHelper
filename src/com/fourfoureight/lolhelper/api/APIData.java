9826cd6fc91443410987dde27ade42190a91226d
Gregory Ervin <kaosuryoko@gmail.com>
2014-09-20 17:40:21 -0500

    Added GSON lib.

---
+++ src/com/fourfoureight/lolhelper/api/APIData.java
@@ -0,0 +1,15 @@
+package com.fourfoureight.lolhelper.api;
+
+
+public class APIData
+{
+	// Private constructor because static class
+	private APIData()
+	{
+	}
+
+	public Champion GetChampionByName(String name)
+	{
+
+	}
+}
