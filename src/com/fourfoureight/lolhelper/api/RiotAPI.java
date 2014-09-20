2fb60b244d21c3a818aa899e109dc2b4bfa7fd40
Gregory Ervin <kaosuryoko@gmail.com>
2014-09-20 17:34:04 -0500

    Improved gitignore. Added database and API code. Static calls to get data from database to be implemented soon.

--- src/com/fourfoureight/lolhelper/api/RiotAPI.java
+++ src/com/fourfoureight/lolhelper/api/RiotAPI.java
@@ -1,98 +1,55 @@
 package com.fourfoureight.lolhelper.api;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Scanner;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class RiotAPI
 {
 	private static final String APIKey = "api_key=f685bd80-568e-4ff3-9d20-4bde13ab7106";
-	/**
-	 * TODO: Update to handle multiple regions.
-	 */
-	private static String BaseAPIAddress = "https://na.api.pvp.net/api/lol/";
+	private final String BaseAPIAddress;
+	private static String StaticDataAPIAddress;
 
 	/**
 	 * Private constructor to prevent initialization of static class.
 	 */
-	private RiotAPI()
+	public RiotAPI(String region)
 	{
+		BaseAPIAddress = "https://" + region + ".api.pvp.net/api/lol/";
+		StaticDataAPIAddress = "static-data/" + region + "/v1.2/";
 	}
 
 	/**
 	 * Takes a string (which represents a URL), opens the URL connection and input stream, then converts the entire
-	 * contents of the result of the UR to a JSONObject. All other methods in this class call this method.
+	 * contents of the result of the UR to a JSONElement. All other methods in this class call this method.
 	 * 
 	 * @param APIQuery
 	 *            The String representing the Riot API URL for the desired call.
 	 * @return
-	 *         A JSONObject containing the results of the Riot API call.
+	 *         A JSONElement containing the results of the Riot API call.
 	 */
-	private static JSONObject CallAPIObject(String APIQuery)
+	private static JsonElement CallAPI(String APIQuery)
 	{
 		try
 		{
-			String APIResult;
+			String APIString;
 			URL APIQueryURL = new URL(APIQuery);
 			InputStream APIQueryIS = APIQueryURL.openStream();
 			Scanner scan = new Scanner(APIQueryIS);
-			APIResult = scan.useDelimiter("\\A").next();
+			APIString = scan.useDelimiter("\\A").next();
 			scan.close();
 			APIQueryIS.close();
-			JSONObject APIResultJSON;
 
-			try
-			{
-				APIResultJSON = new JSONObject(APIResult);
-				return APIResultJSON;
-			} catch (JSONException e)
-			{
-				return null;
-			}
+			JsonElement APIResult;
 
-		} catch (IOException e)
-		{
-			return null;
-		}
-	}
+			APIResult = new JsonParser().parse(APIString);
 
-	/**
-	 * Takes a string (which represents a URL), opens the URL connection and input stream, then converts the entire
-	 * contents of the result of the UR to a JSONObject. All other methods in this class call this method.
-	 * 
-	 * @param APIQuery
-	 *            The String representing the Riot API URL for the desired call.
-	 * @return
-	 *         A JSONArray containing the results of the Riot API call.
-	 */
-	private static JSONArray CallAPIArray(String APIQuery)
-	{
-		try
-		{
-			String APIResult;
-			URL APIQueryURL = new URL(APIQuery);
-			InputStream APIQueryIS = APIQueryURL.openStream();
-			Scanner scan = new Scanner(APIQueryIS);
-			APIResult = scan.useDelimiter("\\A").next();
-			scan.close();
-			APIQueryIS.close();
-			JSONArray APIResultJSON;
+			return APIResult;
 
-			try
-			{
-				APIResultJSON = new JSONArray(APIResult);
-				return APIResultJSON;
-			} catch (JSONException e)
-			{
-				return null;
-			}
-
-		} catch (IOException e)
+		} catch (Exception e)
 		{
 			return null;
 		}
@@ -101,78 +58,76 @@ public class RiotAPI
 	/*
 	 * Beginning of Static API Queries
 	 */
-	private static String StaticDataAPIAddress = "static-data/na/v1.2/";
-
-	public static JSONObject GetChampions()
+	public JsonElement GetChampions()
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "champion?champData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetChampion(int id)
+	public JsonElement GetChampion(int id)
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "champion/" + id + "?champData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetItems()
+	public JsonElement GetItems()
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "item?itemListData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetItem(int id)
+	public JsonElement GetItem(int id)
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "item/" + id + "?itemData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetMasteries()
+	public JsonElement GetMasteries()
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "mastery?masteryListData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetMastery(int id)
+	public JsonElement GetMastery(int id)
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "mastery/" + id + "?masteryData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetRealm()
+	public JsonElement GetRealm()
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "realm?" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetRunes()
+	public JsonElement GetRunes()
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "rune?runeListData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetRune(int id)
+	public JsonElement GetRune(int id)
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "rune/" + id + "?runedata=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetSummonerSpells()
+	public JsonElement GetSummonerSpells()
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "summoner-spell?spellData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONObject GetSummonerSpell(int id)
+	public JsonElement GetSummonerSpell(int id)
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "summoner-spell/" + id + "?spellData=all&" + APIKey;
-		return CallAPIObject(APIQuery);
+		return CallAPI(APIQuery);
 	}
 
-	public static JSONArray GetVersions()
+	public JsonElement GetVersions()
 	{
 		String APIQuery = BaseAPIAddress + StaticDataAPIAddress + "versions?" + APIKey;
-		return CallAPIArray(APIQuery);
+		return CallAPI(APIQuery);
 	}
 	/*
 	 * End of Static API Queries
