89d19e34e887f6342bffa993d853f37503084b6b
Gregory Ervin <kaosuryoko@gmail.com>
2014-09-21 14:18:13 -0500

    APIData Skeleton

--- src/com/fourfoureight/lolhelper/api/APIData.java
+++ src/com/fourfoureight/lolhelper/api/APIData.java
@@ -1,12 +1,24 @@
 package com.fourfoureight.lolhelper.api;
 
-import com.fourfoureight.lolhelper.api.dto.Champion.Champion;
+import com.fourfoureight.lolhelper.api.dto.staticdata.Champion.Champion;
+import com.fourfoureight.lolhelper.api.dto.staticdata.Item.Item;
+import com.fourfoureight.lolhelper.api.dto.staticdata.Mastery.Mastery;
+import com.fourfoureight.lolhelper.api.dto.staticdata.Rune.Rune;
+import com.fourfoureight.lolhelper.api.dto.staticdata.SummonerSpell.SummonerSpell;
 
+/**
+ * @author KaosuRyoko
+ */
 public class APIData
 {
 	// Private constructor because static class
 	private APIData()
 	{
+	}
+
+	public static Champion GetChampionByID(int ID)
+	{
+		return null;
 	}
 
 	public static Champion GetChampionByName(String name)
@@ -17,5 +29,95 @@ public class APIData
 	public static Champion GetChampionByKey(String key)
 	{
 		return null;
+	}
+
+	public static String[] GetChampionList()
+	{
+		return null;
+	}
+
+	public static int GetNumberChampions()
+	{
+		return -1;
+	}
+
+	public static Item GetItemByID(int ID)
+	{
+		return null;
+	}
+
+	public static Item GetItemByName(String name)
+	{
+		return null;
+	}
+
+	public static String[] GetItemList()
+	{
+		return null;
+	}
+
+	public static int GetNumberItems()
+	{
+		return -1;
+	}
+
+	public static Mastery GetMasteryByID(int ID)
+	{
+		return null;
+	}
+
+	public static Mastery GetMasteryByName(String name)
+	{
+		return null;
+	}
+
+	public static String[] GetMasteryList()
+	{
+		return null;
+	}
+
+	public static int GetNumberMasteries()
+	{
+		return -1;
+	}
+
+	public static Rune GetRuneByID(int ID)
+	{
+		return null;
+	}
+
+	public static Rune GetRuneByName(String name)
+	{
+		return null;
+	}
+
+	public static String[] GetRuneList()
+	{
+		return null;
+	}
+
+	public static int GetNumberRunes()
+	{
+		return -1;
+	}
+
+	public static SummonerSpell GetSummonerSpellByID(int ID)
+	{
+		return null;
+	}
+
+	public static SummonerSpell GetSummonerSpellByName(String name)
+	{
+		return null;
+	}
+
+	public static String[] GetSummonerSpellList()
+	{
+		return null;
+	}
+
+	public static int GetNumberSummonerSpells()
+	{
+		return -1;
 	}
 }
